090c83126b943c9c446383de5c29620adcae99b5
SuryaGymnastyar <suryagymnastyarr@gmail.com>
2024-12-09 19:53:21 +0700

    Menyelesaikan percabangan dan menambahkan fungsi list daftar pustaka

--- daftarpustaka.cpp
+++ daftarpustaka.cpp
@@ -40,3 +40,49 @@ void BuatDapus() {
         cin.getline(link, 1000);
 
         myfile << penulis << ".(" << tahun << "). " << judul << ". " << sumber << ". Diakses dari " << link << "." << endl;
+	    
+     } else if (pilihan == 2) { 
+        cout << "Masukkan nama jurnal : ";
+        cin.getline(sumber, 100);
+        cout << "Masukkan volume : ";
+        cin.getline(volume, 10);
+        cout << "Masukkan issue (nomor jurnal) : ";
+        cin.getline(issue, 10);
+        cout << "Masukkan halaman : ";
+        cin.getline(halaman, 20);
+
+        myfile << penulis << ".(" << tahun << "). " << judul << ". " << sumber << ", " << volume << "(" << issue << "), " << halaman << "." << endl;
+
+    } else if (pilihan == 3) { 
+        cout << "Masukkan kota publikasi : ";
+        cin.getline(kota, 50);
+        cout << "Masukkan nama penerbit : ";
+        cin.getline(penerbit, 100);
+
+        myfile << penulis << ".(" << tahun << "). " << judul << ". " << kota << " : " << penerbit << "." << endl;
+
+    } else {
+        cout << "Pilihan tidak ada, coba kembali." << endl;
+        return;
+    }
+
+    myfile.close();
+    cout << "Daftar pustaka berhasil disimpan!" << endl;
+}
+
+void ListDapus() {
+    ifstream ifile;
+	
+	ifile.open("DaftarPustaka.txt", ios::app);
+    if (ifile.fail()) {
+        cout << "Gagal membuka file" << endl;
+        return;
+    }
+
+    char dapus[500];
+    cout << endl << "Daftar Pustaka Tersimpan : " << endl;
+    while (ifile.getline(dapus, 500)) {
+        cout << dapus << endl;
+    }
+    ifile.close();
+}
