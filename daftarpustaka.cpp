556b136e5eb1ce3a55b3569d78c3a47b1953d92f
SuryaGymnastyar <suryagymnastyarr@gmail.com>
2024-12-09 20:39:31 +0700

    Menyelesaikan percabangan dan menambahkan function list dapus

--- daftarpustaka.cpp
+++ daftarpustaka.cpp
@@ -28,7 +28,7 @@ void BuatDapus() {
 
     cout << "Masukkan nama penulis (Nama belakang dahulu, Nama depan) : ";
     cin.getline(penulis, 100);
-	cout << "Masukkan tahun publikasi : ";
+    cout << "Masukkan tahun publikasi : ";
     cin.getline(tahun, 10);
     cout << "Masukkan judul : ";
     cin.getline(judul, 100);
@@ -40,3 +40,48 @@ void BuatDapus() {
         cin.getline(link, 1000);
 
         myfile << penulis << ".(" << tahun << "). " << judul << ". " << sumber << ". Diakses dari " << link << "." << endl;
+
+    } else if (pilihan == 2) { 
+        cout << "Masukkan nama jurnal : ";
+        cin.getline(sumber, 100);
+        cout << "Masukkan volume : ";
+        cin.getline(volume, 10);
+        cout << "Masukkan issue (nomor jurnal) : ";
+        cin.getline(issue, 10);
+        cout << "Masukkan halaman : ";
+        cin.getline(halaman, 20);
+
+        myfile << penulis << ".(" << tahun << "). " << judul << ". " << sumber << ", " << volume << "(" << issue << "), " << halaman << "." << endl;
+
+    } else if (pilihan == 3) { 
+        cout << "Masukkan kota publikasi : ";
+        cin.getline(kota, 50);
+        cout << "Masukkan nama penerbit : ";
+        cin.getline(penerbit, 100);
+
+        myfile << penulis << ".(" << tahun << "). " << judul << ". " << kota << " : " << penerbit << "." << endl;
+
+    } else {
+        cout << "Pilihan tidak ada, coba kembali." << endl;
+        return;
+    }
+    myfile.close();
+    cout << "Daftar pustaka berhasil disimpan!" << endl;
+}
+
+void ListDapus() {
+    ifstream ifile;
+	
+	ifile.open("DaftarPustaka.txt", ios::app);
+    if (ifile.fail()) {
+        cout << "Gagal membuka file" << endl;
+        return;
+    }
+	
+    char dapus[500];
+    cout << endl << "Daftar Pustaka Tersimpan : " << endl;
+    while (ifile.getline(dapus, 500)) {
+        cout << dapus << endl;
+    }
+    ifile.close();
+}
