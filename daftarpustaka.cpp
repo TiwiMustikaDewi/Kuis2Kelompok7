fdace8f5134b6fd3ca93cc99882b02c2990e7bbc
Alyssa <lisxph07@gmail.com>
2024-12-09 19:48:53 +0700

    Menambahkan function buat daftar pustaka

--- daftarpustaka.cpp
+++ daftarpustaka.cpp
@@ -2,3 +2,41 @@
 #include <fstream>
 using namespace std;
 
+void BuatDapus() {
+    ofstream myfile;
+	
+	myfile.open("DaftarPustaka.txt", ios::app);
+	
+    if (myfile.fail()) {
+        cout << "Gagal membuka file" << endl;
+        return;
+    }
+
+    int pilihan;
+    cout << endl << " -------------------------------" << endl;
+    cout << "|          Pilih Sumber         |" << endl;
+    cout << " -------------------------------" << endl;
+    cout << "| 1. Artikel                    |" << endl;
+    cout << "| 2. Jurnal                     |" << endl;
+    cout << "| 3. Buku                       |" << endl;
+    cout << " -------------------------------" << endl;
+    cout << "Masukkan pilihan : ";
+    cin >> pilihan;
+    cin.ignore();
+
+    char penulis[100], tahun[10], judul[100], sumber[100], penerbit[100], volume[10], issue[10], halaman[20], kota[50], link[1000];
+
+    cout << "Masukkan nama penulis (Nama belakang dahulu, Nama depan) : ";
+    cin.getline(penulis, 100);
+	cout << "Masukkan tahun publikasi : ";
+    cin.getline(tahun, 10);
+    cout << "Masukkan judul : ";
+    cin.getline(judul, 100);
+    
+    if (pilihan == 1) { 
+        cout << "Masukkan sumber (Nama Website atau Media) : ";
+        cin.getline(sumber, 100);
+        cout << "Masukkan URL : ";
+        cin.getline(link, 1000);
+
+        myfile << penulis << ".(" << tahun << "). " << judul << ". " << sumber << ". Diakses dari " << link << "." << endl;
